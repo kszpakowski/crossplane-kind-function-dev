d9197499ffb553a205b3133da33f3c559e5c4652
Karol Szpakowski <karol.szpakowski@gmail.com>
2025-05-11 00:34:30 +0200

    clean up

--- install.sh
+++
@@ -1,3 +0,0 @@
-helm repo add crossplane-stable https://charts.crossplane.io/stable
-helm repo update
-helm install -f override.yaml crossplane --namespace crossplane-system --create-namespace crossplane-stable/crossplane
